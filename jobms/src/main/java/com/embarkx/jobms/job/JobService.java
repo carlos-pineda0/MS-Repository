4ebdec17a584895e005d3f034d09f94f84ec2da0
carlos-pineda0 <160975236+carlos-pineda0@users.noreply.github.com>
2024-02-22 17:08:05 -0600

    Job

---
+++ jobms/src/main/java/com/embarkx/jobms/job/JobService.java
@@ -0,0 +1,16 @@
+package com.embarkx.jobms.job;
+
+import java.util.List;
+
+public interface JobService {
+
+    List<Job> findAll();
+    void createJob(Job job);
+
+    Job getJobById(Long id);
+
+    boolean deleteById(Long id);
+
+    boolean updateJob(Long id, Job updatedJob);
+
+}
